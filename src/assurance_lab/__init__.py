7abb966b05db4bfc54c8bea4082e9433564d8246
gyubin02 <dlrbqls980@gmail.com>
2026-07-29 00:50:11 +0900

    Remove superseded assurance prototypes

--- src/assurance_lab/__init__.py
+++ src/assurance_lab/__init__.py
@@ -1,45 +1,6 @@
-"""Core models and evaluators for the control assurance laboratory."""
+"""Evidence-backed security-control experiments."""
 
-from assurance_lab.claims import (
-    Applicability,
-    ClaimEvaluation,
-    Defeater,
-    DefeaterEffect,
-    DisplayState,
-    ExecutionState,
-    QualityState,
-    SupportState,
-)
-from assurance_lab.experiments import (
-    Assertion,
-    CheckRole,
-    ControlExperimentSpec,
-    InterventionalWitnessEvaluator,
-    Relation,
-    RelationOperator,
-    RunRecord,
-    RunRole,
-    WitnessResult,
-    WitnessState,
-)
+from assurance_lab.claim_spec import ClaimSpec
+from assurance_lab.protocol import ExperimentSpec, ProtocolEvaluator
 
-__all__ = [
-    "Applicability",
-    "Assertion",
-    "CheckRole",
-    "ClaimEvaluation",
-    "ControlExperimentSpec",
-    "Defeater",
-    "DefeaterEffect",
-    "DisplayState",
-    "ExecutionState",
-    "InterventionalWitnessEvaluator",
-    "QualityState",
-    "Relation",
-    "RelationOperator",
-    "RunRecord",
-    "RunRole",
-    "SupportState",
-    "WitnessResult",
-    "WitnessState",
-]
+__all__ = ["ClaimSpec", "ExperimentSpec", "ProtocolEvaluator"]
